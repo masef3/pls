e8af76a003b99a09e8bfa51723731968e6da5b2e
masef3 <serafin.matus0@gmail.com>
2026-08-07 12:02:00 +0200

    fix help #2

--- src/cmd_args.py
+++ src/cmd_args.py
@@ -7,12 +7,12 @@ def help_() -> None:
 
     Commands:
         clean       Recycles files into directories that differ by extension
-        change_path Changes working directory of the program
+        change      Changes working directory of the program
         watch       Watches and prints all modifications of wanted directory
         cleanbg     Runs online cleaner
         show        Shows working directory
 
     Flags:
-        -h, --help  help for pls
+        -h, --help, help
     """
     print(msg)
