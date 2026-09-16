4eb7784c8d6169d4ef6bc0a221b15c0775cb8584
masef3 <serafin.matus0@gmail.com>
2026-09-16 16:45:44 +0200

    update pyproject

---
+++ build/lib/main.py
@@ -0,0 +1,59 @@
+import sys
+from dir_cleaner import Cleaner, confirmation, Trigger
+from pathlib import Path
+import watch_module as wm
+from cmd_args import help_
+
+
+def main() -> None:
+    args = sys.argv
+    if len(args) > 3:
+        print("Too many arguments")
+        return
+
+    cleaner = Cleaner()
+
+    if (len(args) < 2):
+        help_()
+        return
+
+    arg = args[1]
+
+    match arg:
+        case "--help" | "-h" | "help":
+            help_()
+
+        case "show":
+            cleaner.show_path()
+
+        case "change":
+            if (len(args) < 3):
+                cleaner.change_path(None)
+            else:
+                cleaner.change_path(Path(args[2]))
+
+        case "clean":
+            inp = confirmation("package")
+            if inp:
+                exts = cleaner.get_ext()
+                cleaner.package(exts)
+
+        case "cleanbg":
+            runner = wm.Runner(cleaner.access_path(), Trigger.OFF_WATCH)
+            runner.start()
+
+        case "watch":
+            inp = confirmation("watch")
+            if inp:
+                runner = wm.Runner(cleaner.access_path(), Trigger.ON_WATCH)
+                runner.start()
+
+        case "recycle":
+            cleaner.recycle()
+
+        case _:
+            print("No match for given argument")
+
+
+if __name__ == "__main__":
+    main()
---
+++ build/lib/watch_module.py
@@ -0,0 +1,69 @@
+from watchdog.observers import Observer
+from pathlib import Path
+from watchdog.events import FileSystemEventHandler, FileSystemEvent
+import time
+from dir_cleaner import Cleaner, clean, Trigger
+
+
+class Logger(FileSystemEventHandler):
+
+    def __init__(self, on_watch: Trigger) -> None:
+        super().__init__()
+        self.on_watch = on_watch
+        self.call_flag: bool = False
+
+    def on_any_event(self, event: FileSystemEvent) -> None:
+        if self.on_watch:
+            match event.event_type:
+                case "created":
+                    print("Created file/directory: ", event.src_path)
+                case "modified":
+                    print("Modified file/directory: ", event.src_path)
+                case "deleted":
+                    print("Deleted file/directory: ", event.src_path)
+                case "moved":
+                    print("Moved file/directory", event.src_path)
+
+        elif not self.call_flag:
+            self.call_flag = True
+            try:
+                cleaner = Cleaner()
+                match event.event_type:
+                    case "created" | "modified":
+                        clean(cleaner)
+            finally:
+                self.call_flag = False
+
+        elif self.call_flag:
+            return
+
+
+class Wd_(Logger):
+    def __init__(self, wd_path: Path, on_watch: Trigger) -> None:
+        super().__init__(on_watch)
+        self.__wd_path = wd_path
+        self.observer = Observer()
+
+    def watch(self) -> None:
+        """
+        only spectates and reports the interactions with given directory.
+        """
+        self.observer.schedule(self, self.__wd_path, recursive=False)
+        self.observer.start()
+
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            self.observer.stop()
+
+
+class Runner(Wd_):
+    def __init__(self, wd_path: Path, on_watch: Trigger) -> None:
+        super().__init__(wd_path, on_watch)
+
+    def start(self) -> None:
+        self.watch()
+
+    def stop(self) -> None:
+        self.observer.stop()
